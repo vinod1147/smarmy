a53b80068d1de99840aad0c3977e6ecd8fc0205b
Vinod <72577940+vinod1147@users.noreply.github.com>
2023-04-30 02:45:34 +0530

    version 1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return <>
+    {/* <Head /> */}
     <Navbar />
     <Component {...pageProps} />
     <Footer />
